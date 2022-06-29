b5acd1179b01b99ae45c84ff63b8c88a52b3cb5a
Trent Brew <32501733+trentbrew@users.noreply.github.com>
2022-06-28 19:41:47 -0500

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev" class="underline">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-[36px]">Welcome to SvelteKit</h1>
+<p class="opacity-50">Visit <a href="https://kit.svelte.dev" class="underline">kit.svelte.dev</a> to read the documentation</p>
